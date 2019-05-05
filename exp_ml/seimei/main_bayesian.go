6a20ddbc4a996dcff082f8a199637c5133081bca
tesujiro <tesujiro@gmail.com>
2019-05-05 15:55:47 +0900

    add: split Japanese Surname and Givenname with naive bayesian

---
+++ exp_ml/seimei/main_bayesian.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	. "github.com/jbrukh/bayesian"
+)
+
+const (
+	TRAINING_DATA   = "./train.txt"
+	EVALUATION_DATA = "./eval.txt"
+)
+
+const (
+	CHAR_1 Class = "1"
+	CHAR_2 Class = "2"
+	CHAR_3 Class = "3"
+	CHAR_4 Class = "4"
+	CHAR_5 Class = "5"
+)
+
+func learn(c *Classifier) error {
+	file, err := os.Open(TRAINING_DATA)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	r := csv.NewReader(file)
+	r.Comma = ':'
+	r.LazyQuotes = true
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		//for i, v := range record {
+		//fmt.Printf("col[%v]:%v\t", i, v)
+		//}
+		DIVIDE_NUM := record[3]
+		NAME := record[2]
+		LEARN_DATA := []string{}
+		for _, rn := range NAME {
+			//fmt.Printf("[%v]:%c\t", i, rn)
+			LEARN_DATA = append(LEARN_DATA, fmt.Sprintf("%c", rn))
+		}
+		//fmt.Printf("DIV=%v,LEARN_DATA=%v\n", DIVIDE_NUM, LEARN_DATA)
+		c.Learn(LEARN_DATA, Class(DIVIDE_NUM))
+	}
+
+	return nil
+}
+
+func evaluate(c *Classifier) error {
+	file, err := os.Open(EVALUATION_DATA)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	r := csv.NewReader(file)
+	r.Comma = ':'
+	r.LazyQuotes = true
+
+	var right, wrong int
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		DIVIDE_NUM := record[3]
+		NAME := record[2]
+		LEARN_DATA := []string{}
+		for _, rn := range NAME {
+			//fmt.Printf("[%v]:%c\t", i, rn)
+			LEARN_DATA = append(LEARN_DATA, fmt.Sprintf("%c", rn))
+		}
+
+		//scores, likely, _ := c.LogScores(LEARN_DATA)
+		//fmt.Printf("name=%v\tscores=%v\tlikely=%v\n", NAME, scores, likely)
+
+		correct, err := strconv.Atoi(DIVIDE_NUM)
+		if err != nil {
+			fmt.Printf("Number conversion error:%v", err)
+			os.Exit(1)
+		}
+		probs, likely, _ := c.ProbScores(LEARN_DATA)
+		if correct != likely+1 {
+			fmt.Printf("name=%v\tprobs=%8.8f\tlikely=%8.8d\n", NAME, probs, likely)
+			wrong++
+		} else {
+			right++
+		}
+	}
+	fmt.Printf(" Collect Ratio %v / %v = %v%\n", right, right+wrong, right*100/(right+wrong))
+	return nil
+}
+
+func main() {
+	classifier := NewClassifier(CHAR_1, CHAR_2, CHAR_3, CHAR_4, CHAR_5)
+	if err := learn(classifier); err != nil {
+		fmt.Println("Training ERROR :", err)
+		os.Exit(1)
+	}
+
+	if err := evaluate(classifier); err != nil {
+		fmt.Println("Evaluation ERROR :", err)
+		os.Exit(1)
+	}
+
+	/*
+		scores, likely, _ := classifier.LogScores([]string{"tall", "girl"})
+		fmt.Printf("scores=%v\tlikely=%v\n", scores, likely)
+
+		probs, likely, _ := classifier.ProbScores([]string{"tall", "girl"})
+		fmt.Printf("probs=%8.8f\tlikely=%8.8d\n", probs, likely)
+	*/
+}
---
+++ exp_ml/seimei/random_name.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+INFILE=./JINMEI30.TXT
+#OUTFILE=./name_dictionary.txt
+REPEAT=${1:-1000}
+
+conv()
+{
+    iconv -f CP932 -t UTF8 $1 | sed 's/'$'\r//g'
+}
+
+format()
+{
+    gawk '
+    BEGIN{
+    	OFS=":"
+    }
+    NF==2 {
+	KANA=$1
+	n=split($2,a,":")
+	if(n!=2) next;
+	KANJI=a[1]
+	gsub(/"/,"",KANJI)
+	KIND=a[2]
+	if(KANJI==""){
+	    KANJI="ERROR NR:" NR
+	}
+	if(KANA==""){
+	    KANA="ERROR NR:" NR
+	}
+	if(KIND=="姓"||KIND=="名"){
+	    print KIND,KANJI,KANA
+	}
+    }'
+}
+
+random_name()
+{
+    gawk -F: -v REPEAT=$1 '
+    {
+	KIND=$1
+	NAME=$2
+	KANA=$3
+	if(KIND=="姓"){
+	    SNAME_COUNT++
+	    SNAME_KANJI[SNAME_COUNT]=NAME
+	    SNAME_KANA[SNAME_COUNT]=KANA
+	} else {
+	    GNAME_COUNT++
+	    GNAME_KANJI[GNAME_COUNT]=NAME
+	    FNAME_KANA[GNAME_COUNT]=KANA
+	}
+    }
+    END{
+    	OFS=":"
+	srand()
+	for(i=1;i<=REPEAT;i++){
+	    RND_SNAME=SNAME_KANJI[int(rand()*SNAME_COUNT)+1]
+	    RND_GNAME=GNAME_KANJI[int(rand()*GNAME_COUNT)+1]
+	    FULLNAME=RND_SNAME RND_GNAME
+	    DIV=length(RND_SNAME)
+	    print RND_SNAME,RND_GNAME,FULLNAME,DIV
+	}
+
+    }'
+}
+
+unzip -qq ${INFILE}.zip 
+conv $INFILE | format | random_name $REPEAT
+rm $INFILE
