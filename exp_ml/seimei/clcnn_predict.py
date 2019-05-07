c7e5e0f2d2eb41cf331e53f5ff9c031dd72326b2
tesujiro <tesujiro@gmail.com>
2019-05-07 20:54:32 +0900

    perfomance improvement

--- exp_ml/seimei/clcnn_predict.py
+++ exp_ml/seimei/clcnn_predict.py
@@ -33,43 +33,25 @@ if __name__ == "__main__":
 
     input_values = []
     target_values = []
+    right_count = 0
+    wrong_count = 0
+    model_filepath="model.h5"
+    model = load_model(model_filepath)
     for target_value, input_value in name_list:
         input_values.append(input_value)
         #target_values.append(target_value)
         #target_values.append(np.identity(5)[target_value])
         target_values = np.identity(5)[target_value]
         name = "".join([chr(i) for i in input_value])
-        ret = predict(np.array([input_value]))
+        ret = model.predict(np.array([input_value]))
+        #ret = model.predict(name_list)
         expect = np.argmax(target_values)
         actual = np.argmax(ret)
         if (expect==actual):
-            print("NAME:{}\tDIV={}".format(name,actual+1))
+            #print("NAME:{}\tDIV={}".format(name,actual+1))
+            right_count+=1
         else:
             print("NAME:{}\texpect:{}\tactual:{}".format(name,expect+1,actual+1))
-        #print("".format(target_values))
-        #print("          ret=={}".format(np.argmax(ret)))
-
-    '''
-    print("input_values={}".format(input_values))
-    results = predict(input_values)
-    for result in result:
-        preint("result={}".format(result))
-
-    right_count = 0
-    wrong_count = 0
-    '''
-
-    
-    '''
-    raw_comment = "デートには最高やで！"
-    comment = [ord(x) for x in raw_comment.strip().decode("utf-8")]
-    comment = comment[:300]
-    if len(comment) < 10:
-        exit("too short!!")
-    if len(comment) < 300:
-        comment += ([0] * (300 - len(comment)))
-    ret = predict(np.array([comment]))
-    predict_result = ret[0][0]
-    print "リア充度: {}%".format(predict_result * 100)
-    '''
+            wrong_count+=1
+    print("正解率:{}%".format(right_count*100/(right_count+wrong_count)))
 
